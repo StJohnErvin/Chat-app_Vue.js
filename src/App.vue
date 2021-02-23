28f1396e70dd813bd5c4c9087336830faed2917f
acal <acal@matrixgroup.net>
2021-02-23 14:51:41 +0800

    remove class in default app

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,15 @@ export default {
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
